f895bc9d58d6f182bf54c8ac78a693cb85d32d3b
000 <000@192.168.1.3>
2014-11-25 20:16:37 +0200

    implementation of swipe in main activity

--- SwipeApp/src/com/example/swipeapp/MainActivity.java
+++ SwipeApp/src/com/example/swipeapp/MainActivity.java
@@ -1,34 +1,108 @@
 package com.example.swipeapp;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity
+{
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    DemoCollectionPagerAdapter mDemoCollectionPagerAdapter;
+    
+    ViewPager mViewPager;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    public void onCreate( Bundle savedInstanceState )
+    {
+    
+    	super.onCreate( savedInstanceState );
+    	
+        setContentView( R.layout.activity_collection_demo );
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mDemoCollectionPagerAdapter = new DemoCollectionPagerAdapter( getSupportFragmentManager() );
+        
+        mViewPager = ( ViewPager ) findViewById( R.id.pager );
+        
+        mViewPager.setAdapter( mDemoCollectionPagerAdapter );
+    
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+
+    // Since this is an object collection, use a FragmentStatePagerAdapter,
+    // and NOT a FragmentPagerAdapter.
+    public class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter
+    {
+    
+    	public DemoCollectionPagerAdapter( FragmentManager fm )
+    	{
+    		
+    		super(fm);
+
+    	}
+
+	    public Fragment getItem( int i )
+	    {
+	    
+	    	Fragment fragment = new DemoObjectFragment();
+	        
+	    	Bundle args = new Bundle();
+	        // Our object is just an integer :-P
+	        
+	    	args.putInt( DemoObjectFragment.ARG_OBJECT, i + 1 );
+	        
+	    	fragment.setArguments( args );
+	        
+	    	return fragment;
+	    
+	    }
+	
+	    @Override
+	    public int getCount()
+	    {
+	    
+	    	return 100;
+	    
+	    }
+	
+	    @Override
+	    public CharSequence getPageTitle( int position )
+	    {
+	    
+	    	return "OBJECT " + ( position + 1 );
+	    
+	    }
+	    
+    }
+
+    // Instances of this class are fragments representing a single
+    // object in our collection.
+	class DemoObjectFragment extends Fragment
+	{
+    
+		public static final String ARG_OBJECT = "object";
+
+		@Override
+		public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
+		{
+        
+			// The last two arguments ensure LayoutParams are inflated
+			// properly.
+			View rootView = inflater.inflate( R.layout.fragment_collection_object, container, false );
+			
+			Bundle args = getArguments();
+			
+			( ( TextView ) rootView.findViewById( android.R.id.text1 ) ).setText( Integer.toString( args.getInt( ARG_OBJECT ) ) );
+			
+			return rootView;
 		}
-		return super.onOptionsItemSelected(item);
 	}
 }
